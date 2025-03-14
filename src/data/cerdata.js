dd40957b9c716e48a9c812d066a73e53ff81f515
Aayush Dongol <ayussh222dongol@gmail.com>
2025-03-13 20:46:57 -0400

    basic changes

---
+++ src/data/cerdata.js
@@ -0,0 +1,25 @@
+import LinkedIn from "../images/linkedin.jpg";
+const cerdata = [
+  {
+    id: 1,
+    icon: LinkedIn,
+    name: "Data Science Foundation: Data Engineering",
+  },
+  {
+    id: 2,
+    icon: LinkedIn,
+    name: "SQL Programming",
+  },
+  {
+    id: 3,
+    icon: LinkedIn,
+    name: "Data Science Foundation: Data Engineering",
+  },
+  {
+    id: 4,
+    icon: LinkedIn,
+    name: "Data Science Foundation: Data Engineering",
+  },
+];
+
+export default cerdata;
--- src/pages/Certifications.js
+++ src/pages/Certifications.js
@@ -1,26 +1,32 @@
 import React from "react";
 import Main from "../layouts/Main";
-
+import cerdata from "../data/cerdata";
+import { width } from "@fortawesome/free-brands-svg-icons/faGithub";
 const Certifications = () => {
+  console.log(cerdata, "data");
   return (
     <div>
       <Main title="About" description="Learn about Aayush Dongol">
         <div>
           <h1>My Certifications</h1>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <div>
-              <b>Data Science Foundation: Data Engineering</b>
-              <div
-                style={{ display: "flex", alignItems: "center", gap: "10px" }}
-              >
-                Issuing Organization{" "}
-                <img
-                  style={{ height: "50px", width: "50px" }}
-                  src="https://via.placeholder.com/15"
-                />
+          <div style={{ display: "flex", flexWrap: "column", gap: "20px",width:"100%" }}>
+            {cerdata?.map((details) =>
+              <div style={{ display: "flex", justifyContent: "space-between",width:"100%" }}>
+                <div>
+                  <b>{details?.name}</b>
+                  <div
+                    style={{ display: "flex", alignItems: "center", gap: "10px" }}
+                  >
+                    Issuing Organization
+                    <img
+                      style={{ height: "50px", width: "50px" }}
+                      src={details?.icon}
+                    />
+                  </div>
+                </div>
+                <button>View</button>
               </div>
-            </div>
-            <button>View</button>
+            )}
           </div>
         </div>
       </Main>
